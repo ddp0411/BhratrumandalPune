39c36dedebc27c22828b51cb0283c09936ee8176
Devanshu Patil <devanshupatil1712@gmail.co>
2025-12-11 18:45:47 +0530

    Initial Commit

---
+++ BhratrumandalPune/BhratrumandalPuneApp.swift
@@ -0,0 +1,17 @@
+//
+//  BhratrumandalPuneApp.swift
+//  BhratrumandalPune
+//
+//  Created by Devanshu Patil on 11/12/25.
+//
+
+import SwiftUI
+
+@main
+struct BhratrumandalPuneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
